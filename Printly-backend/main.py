c93a67734f2e3912f0d5e65829707a714a9e3586
Mahmoud El-Saeed <eng.mahmoud.cse@gmail.com>
2026-06-12 19:55:55 +0300

    feat(docker): add Docker support with backend and frontend configurations

--- Printly-backend/main.py
+++ Printly-backend/main.py
@@ -58,12 +58,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost",
+        "http://127.0.0.1",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 async def read_root():
